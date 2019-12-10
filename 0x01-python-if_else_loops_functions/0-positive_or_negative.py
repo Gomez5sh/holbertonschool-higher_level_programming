2138e7572369232edf8add3ea1f6863facffad9b
g5sh <eagomez2387@gmail.com>
2019-12-10 18:55:21 -0500

    [FIX]: Fix the point 0 *7

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-10000, 10000)
+number = random.randint(-10, 10)
 
 if number > 0:
-    print("{} is positive\n".format(number))
+    print("{} is positive".format(number))
 elif number == 0:
-    print("{} is negative\n".format(number))
+    print("{0} is zero".format(number))
 elif number < 0:
-    print(" is zero\n".format(number))
+    print("{} is negative".format(number))
