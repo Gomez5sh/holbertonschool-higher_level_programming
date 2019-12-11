029506c9d9cc63ea8a88047f4d7fa0df78279501
g5sh <eagomez2387@gmail.com>
2019-12-10 20:11:34 -0500

    [FIX]: Fix the point 1 * 4

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,7 +10,6 @@ else:
 if i > 5:
     print("Last digit of {0} is {1} and is greater than 5".format(number, i))
 elif i < 6 and i != 0:
-    print("Last digit of {0} is {1} and is less\
-    than 6 and not 0".format(number, i))
+    print("Last digit of {0} is {1} and is less\ than 6 and not 0".format(number, i))
 elif i == 0:
     print("Last digit of {0} is {1} and is 0".format(number, i))
