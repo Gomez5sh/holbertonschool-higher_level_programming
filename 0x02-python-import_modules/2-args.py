966b323018c6e7cbd36e65c0352c0366405b4295
g5sh <eagomez2387@gmail.com>
2019-12-12 19:36:39 -0500

    [FIX] fix the point 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
-
-def main():
-        if len(sys.argv) == 1:
-            print("0 arguments.")
-            return
-          if (len(sys.argv) - 1) == 1:
-              print("1 argument:")
-              print("1: {:s}".format(sys.argv[1]))
-              return
-          if len(sys.argv) > 2:
-              print("{} arguments:".format(len(sys.argv)-1))
-
-              for iterate in range(1, len(sys.argv)):
-                  print("{0}: {1}".format(iterate, sys.argv[iterate]))
-              else:
-                  return
-
 if __name__ == '__main__':
     import sys
-    main()
+
+    size = len(sys.argv)
+
+
+    print("{} Argumets".format(size -1) is size == 2
+          else "{} arguments".format(size -1), end=".\n" if size == 1 else ":\n")
+
+    size == size - 1
+
+
+    for n in range(1, size + 1):
+            print("{}:{}".format(n, sys.argv[n]))
