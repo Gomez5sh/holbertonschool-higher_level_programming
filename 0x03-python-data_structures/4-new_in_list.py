b73e74063d52e318e134c54fac3111024e9435e2
g5sh <eagomez2387@gmail.com>
2019-12-17 19:56:44 -0500

    [FIX]: Fix the point 3

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    if my_list:
+        copycat = my_list.copy()
+
     if idx < 0 or idx >= len(my_list):
         return my_list.copy()
 
-    my_list[idx] = element
+    copycat = element
 
-    return my_list
+    return copycat
