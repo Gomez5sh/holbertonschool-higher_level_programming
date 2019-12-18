29c8afa9c8c39f8789b382e7bc58e1a097014e86
g5sh <eagomez2387@gmail.com>
2019-12-17 23:42:56 -0500

    [FIX]: Fix point 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-        new_list = [True if (i % 2) == 0 else False for i in my_list]
-            return (new_list)
+        save = []
+        for i in my_list:
+                if i % 2 == 0:
+                        i = True
+                        save.append(i)
+                else:
+                        i = False
+                        save.append(i)
+
+                        return save
