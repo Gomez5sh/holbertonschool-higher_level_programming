afb2f1f8eb060290237e0041735b85bdbc255398
g5sh <eagomez2387@gmail.com>
2019-12-19 15:18:50 -0500

    [FIX]: Fix point 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    uniq = {}
-    for a in my_list:
-        if a not in uniq:
-            uniq[a] = 1
-        else:
-            uniq[a] += 1
-    summ = 0
-    for it in uniq:
-        summ = summ = 1
+    uniq = set(my_list)
+    summ = sum(uniq)
+    
     return summ
