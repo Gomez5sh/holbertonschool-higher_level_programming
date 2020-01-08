f05b997ee6bd8480645646bb601c17ffa72a0b92
g5sh <eagomez2387@gmail.com>
2020-01-08 15:47:19 -0500

    [ADD]: file 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    con = 0
+    for n in range(x):
+        try:
+            print("{:d}".fotmat(my_list[n]), end="")
+        except (ValueError, TypeError):
+            continue
+        else:
+            con += 1
+        print()
+        return(con)
