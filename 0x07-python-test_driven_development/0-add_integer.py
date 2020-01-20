425a4e43d0843c89fc2995e5b6989e55093285d4
g5sh <eagomez2387@gmail.com>
2020-01-20 08:15:45 -0500

    [FIX]: Fix the point 0 * 29

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
-    """Sum of a int
-    b and a = int
-    """
+    """Sum of a int b and a = int"""
     if type(a) != float and type(a) != int:
         raise TypeError("a must be an integer")
     elif type(b) != float and type(b) != int:
         raise TypeError("b must be an integer")
     else:
-        sum = int(a) + int(b)
-        return sum
+        sum1 = int(a)
+        sum2 = int(b)
+        return sum1 + sum2
