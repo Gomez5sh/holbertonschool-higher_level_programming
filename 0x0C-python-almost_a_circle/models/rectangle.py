dee2f3fe7fe08b2e795a224883ad9258ab26c659
g5sh <eagomez2387@gmail.com>
2020-01-27 17:31:23 -0500

    [FIX]: Fix base file * 6

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -91,8 +91,8 @@ class Rectangle(Base):
 
     def __str__(self):
         """str returns"""
-        print("[Rectangle] ({}) {}/{} - {}/{}"
-              .format(self.id, self.x, self.y, self.width, self.height))
+        return('[Rectangle] ({}) {}/{} - {}/{}'
+                       .format(self.id, self.x, self.y, self.width, self.height))
 
     def update(self, *args):
         """assigns arguments"""
