f3f34083bfcd0f29ad4dae6a58dc2c977481c72f
g5sh <eagomez2387@gmail.com>
2020-03-03 15:46:59 -0500

    [UPDATE]PROJECT: Update the point 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,4 @@
 --creates the database and use
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-GRANT USAGE ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
-SET PASSWORD FOR 'user_0d_2'@'localhost' = PASSWORD('user_0d_2_pwd');
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost'
+      IDENTIFIED BY 'user_0d_2_pwd';
