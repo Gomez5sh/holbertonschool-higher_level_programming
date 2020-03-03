9c9b3c63251ebe4c4f53724bd3ec23d2e3f9aed2
g5sh <eagomez2387@gmail.com>
2020-03-03 15:24:41 -0500

    [FIX]TAKS 1: Fix the point

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- creates the MySQL server user
-CRATE USER IF NOT EXISTS 'user_0d_1'@localhost;
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@localhost;
+CRATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
