48c55d1335a43da9396eb4b3339bde055a31e05e
g5sh <eagomez2387@gmail.com>
2020-03-03 15:28:44 -0500

    [FIX]TAKS 1: Fix the point

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- creates the MySQL server user
 CRATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+SET PASSWORD FOR 'user_0d_1'@'localhost' = PASSWORD('user_0d_1_pwd');
