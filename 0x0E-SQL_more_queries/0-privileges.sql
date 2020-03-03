61b5d2f258401cec854df5005a51676e729005de
g5sh <eagomez2387@gmail.com>
2020-03-03 15:11:17 -0500

    [FIX]TAKS 0: Fix the point

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- Show all privileges for user 1
 -- Show all privileges for user 2
-SHOW GRANTS FOR user_0d_1@localhos;
-SHOW GRANTS FOR user_0d_2@localhos;
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
