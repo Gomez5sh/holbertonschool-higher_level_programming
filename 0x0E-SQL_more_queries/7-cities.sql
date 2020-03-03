0f3d6b98c101383001ec44b0cb642d9753bd94ea
g5sh <eagomez2387@gmail.com>
2020-03-03 17:05:02 -0500

    [UPDATE]POINT 7: Update the point 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,6 +3,6 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities
 (id INT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
 state_id INT NOT NULL,
-name NOT NULL VARCHAR(256),
+name  VARCHAR(256) NOT NULL,
 INDEX state_id (state_id),
 FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id));
