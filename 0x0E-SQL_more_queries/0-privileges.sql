1c8a280e5efc7dceb4f35bdbaefd1fe5b008c0c1
g5sh <eagomez2387@gmail.com>
2020-03-03 15:06:10 -0500

    [FIX]TAKS 0: Add coments

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- Show all privileges of the MySQL users
+-- Show all privileges for user 1
 SHOW GRANTS FOR user_0d_1@localhos;
+-- Show all privileges for user 2
 SHOW GRANTS FOR user_0d_2@localhos;
