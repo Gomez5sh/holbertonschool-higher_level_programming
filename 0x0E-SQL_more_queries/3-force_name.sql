2c78df258a671d6aca46b692a9e13db0b72d4281
g5sh <eagomez2387@gmail.com>
2020-03-03 16:13:59 -0500

    [FIX]POINT 3: FIX the point 3*4

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,2 +1,5 @@
 --c3-force_name.sql
-CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS force_name (
+       	     	    	   id INT,
+			   name VARCHAR(256) NOT NULL
+);
