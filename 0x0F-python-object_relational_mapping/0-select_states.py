1781a7e0df9d5144cb205a62536a9fb73940dfeb
g5sh <eagomez2387@gmail.com>
2020-03-29 12:16:35 -0500

    [ADD] POINT: Add point 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,30 +1,24 @@
 #!/usr/bin/python3
 """lists all states from the database hbtn_0e_0_usa"""
-import sys
+
+
+from sys import argv
 import MySQLdb
 
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    db = MySQLdb.connect(host="localhost",
+                         user=username,
+                         passwd=password,
+                         db=db_name,
+                         port=3306)
 
-
-def sqlmain(argv):
-
-    if len(argv) -1 != 3:
-        print("You must enter 3 arguments")
-        return
-
-    db = MySQLd.connect(host="localhost",
-                        user=argv[1],
-                        password=argv[2],
-                        db=argv[3],
-                        port=3306)
     cur = db.cursor()
-    cur.execute("SELECT id, name FROM states ORDER BY id  ASC;")
+    cur.execute("SELECT states.id, name FROM states ORDER BY states.id ASC;")
 
-    for dbi in cur.fetchall():
+    for dbi in cur:
         print(dbi)
-
-    db.close()
-
-if __name__=="__main__":
-
-    import sys
-    sqlmain(sys.argv)
+else:
+    pass
