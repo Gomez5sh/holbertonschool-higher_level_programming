72a31edb9e2c4159e3313a23c202e9bc600537bf
g5sh <eagomez2387@gmail.com>
2020-03-29 12:33:57 -0500

    [ADD] POINT: Add point 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,18 +2,25 @@
 """lists all states with a name starting with N"""
 
 
-from sys import argv
 import MySQLdb
+from sys import argv
+
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host='localhost', user=argv[1],
+
+        db = MySQLdb.connect(host='localhost', user=argv[1],
                              passwd=argv[2], db=argv[3], port=3306)
-    cur = db.cursor()
 
-    cur.execute("SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+        cur = db.cursor()
 
-    for dbi in cur.fetchall():
-        print(dbi)
+        cur.execute("SELECT id, name FROM states WHERE name RLIKE '^N|^n'\
+        ORDER BY states.id ASC;")
 
-    cur.close()
-    db.close()
+
+        query = cur.fetchall()
+
+        for result in query:
+            print(result)
+
+        cur.close()
+        db.close()
