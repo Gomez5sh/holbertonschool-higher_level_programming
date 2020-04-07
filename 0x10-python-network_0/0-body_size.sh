f6263756e399b81de9a430629b2238497a9240ac
g5sh <eagomez2387@gmail.com>
2020-04-07 13:16:58 -0500

    [UPDATE]POINT 1 and 2: Add curl point

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 # Bash script that takes in a URL
 
-curl -sI $1 | grep 'Content-Length' | cut -f2 -d ':'
+curl -sI "$1" | grep 'Content-Length' | cut -f2 -d ':'
---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+# Bash script that sends a DELETE request to the URL
+
+curl -sX DELETE "$1"
