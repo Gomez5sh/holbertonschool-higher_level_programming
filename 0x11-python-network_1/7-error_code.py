92e175c47b8163c5030094487920d5919a0e58b9
g5sh <eagomez2387@gmail.com>
2020-04-15 14:50:35 -0500

    [ADD]POINT 7: Add point 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,10 +6,10 @@ import requests
 import sys
 
 
-if __name__:"__main__":
+if __name__=="__main__":
     res = requests.get(sys.argv[1])
     code_stat = res.status_code
     if code_stat >= 400:
-        print("Error code: {:d}".format(code_stat))
+        print("Error code: {}".format(code_stat))
     else:
         print(res.text)
