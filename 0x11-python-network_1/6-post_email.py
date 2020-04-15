fa8396c3302fdc1b17fc95dcfd94ae2338a6ad13
g5sh <eagomez2387@gmail.com>
2020-04-15 14:30:22 -0500

    [ADD]POINT 6: Add point 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL and an email address"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    email = {'email': sys.argv[2]}
+    res = requests.post(sys.argv[1], data=email)
+    print(res.text)
