7ca92f2bdd4eef333bbf9ffbc332da994f1fb87a
g5sh <eagomez2387@gmail.com>
2020-04-15 13:07:19 -0500

    [FIX]POINT 2: Fix point 2

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -7,10 +7,8 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     try:
-        req: urllib.request.Request(url)
         with urllib.request.urlopen(req) as response:
             html = response.read().decode('utf-8')
             print(html)
     except urllib.error.HTTPError as e:
-        error_code = e.code
-        print("Error code: {:d}".format(error_code))
+        print("Error code: {:d}".format(e.code))
