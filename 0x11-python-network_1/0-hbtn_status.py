f91c80ec329559a20485a58ee1eda3a7ad89f87d
g5sh <eagomez2387@gmail.com>
2020-04-15 12:48:14 -0500

    [FIX]POINT 0: Fix point 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,12 +2,12 @@
 """Python script that fetches"""
 import urllib.request
 
-
-url = urllib.request.Request('https://intranet.hbtn.io/status')
-with urllib.request.urlopen(url) as response:
-    html = response.read()
-    print("Body response")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    html = html.decode('utf-8')
-    print("\t- utf8 content: {}".format(html))
+if __name__ == "__main__":
+    url = urllib.request.Request('https://intranet.hbtn.io/status')
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        print("Body response")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        html = html.decode('utf-8')
+        print("\t- utf8 content: {}".format(html))
