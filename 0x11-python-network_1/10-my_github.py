c454bd09e95c73c239b7616d4d63a44f5c0b46d1
g5sh <eagomez2387@gmail.com>
2020-04-16 12:38:07 -0500

    [FIX]POINT 9: Fix point 9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,11 +4,10 @@ import requests
 import sys
 
 if __name__ == "__main__":
-
     user = sys.argv[1]
     passwd = sys.argv[2]
     res = requests.get("https://api.github.com/user",
-                     auth=(sys.argv[1], sys.argv[2]))
+                       auth=(user, passwd))
     payload = res.json()
     id = payload.get('id')
     print(id)
