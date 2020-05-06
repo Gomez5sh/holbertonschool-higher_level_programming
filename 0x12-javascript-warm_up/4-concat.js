fa138bc2ea5fd0cfc38e9fed1c5b0d2b1fdf54f2
g5sh <eagomez2387@gmail.com>
2020-05-05 19:53:04 -0500

    [ADD] FEATURE: Add point 4

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/*
+Script that prints two arguments passed to it, in the following format: “ is ” */
+const y = process.argv[2];
+const x = process.argv[3];
+
+console.log(y + ' is ' + x);
