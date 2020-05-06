395e06f17e395763d51450a71adacb9865c549f7
g5sh <eagomez2387@gmail.com>
2020-05-05 19:26:27 -0500

    [FIX] FIX: fix ponit 0

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
 /*
 Write a script that prints a message
-depending of the number of arguments passed
-*/
+depending of the number of arguments passed */
 const myVar = process.argv.length;
 
 if (myVar < 3) {
- console.log('No argument');
+  console.log('No argument');
 } else if (myVar === 3) {
- console.log('Argument found');
+  console.log('Argument found');
 } else {
- console.log('Arguments found')
+  console.log('Arguments found');
 }
