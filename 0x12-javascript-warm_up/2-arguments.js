52a1fe8df08adc1a47f47f417e95b8bdad298ed5
g5sh <eagomez2387@gmail.com>
2020-05-05 19:21:00 -0500

    [ADD] FEATURE: Add point 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/*
+Write a script that prints a message
+depending of the number of arguments passed
+*/
+const myVar = process.argv.length;
+
+if (myVar < 3) {
+  console.log('No argument');
+} else if (myVar === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found')
+}
