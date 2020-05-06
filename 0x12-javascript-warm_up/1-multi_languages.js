4ccaa002ac9a7df24ad152d21c97ff7d87020e9a
g5sh <eagomez2387@gmail.com>
2020-05-05 19:07:19 -0500

    [ADD] FEATURE: Add point 1 * 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 //Write a script that prints 3 lines
-const myVar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const c = 'C is fun';
+const py = 'Python is cool';
+const js = 'Javascript is amazing';
 
-console.log(myVar.join('\n'));
+console.log(c);
+console.log(py);
+console.log(js);
