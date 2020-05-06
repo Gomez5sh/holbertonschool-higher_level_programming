6ed1d77893666afe54bce468fadeaffb7caf02d7
g5sh <eagomez2387@gmail.com>
2020-05-05 20:03:35 -0500

    [ADD] FEATURE: Add point 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/*
+Write a script that prints 3 lines
+but by using an array of string and a loop */
+const myVar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+for (let iter = 0; iter in myVar; iter++) {
+  console.log(myVar[iter]);
+}
