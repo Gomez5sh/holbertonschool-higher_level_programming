cbc0cdd494435db9ac926858a51089db40df9f58
g5sh <eagomez2387@gmail.com>
2020-05-05 21:16:19 -0500

    [ADD] FEATURE: Add point 13

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /*
-returns the addition of two ints*/
+returns the addition of two ints */
 
 exports.add = function add (myVar1, myVar2) {
   return parseInt(myVar1) + parseInt(myVar2);
