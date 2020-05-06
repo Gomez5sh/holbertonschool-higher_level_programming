e866eab4dbbd8ebb2058a0686bd1de1213355104
g5sh <eagomez2387@gmail.com>
2020-05-05 19:08:49 -0500

    [ADD] FEATURE: Add point 1 * 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
-//Write a script that prints 3 lines
+/*
+Write a script that prints 3 lines
+*/
 const c = 'C is fun';
 const py = 'Python is cool';
 const js = 'Javascript is amazing';
