5d0b0197cc28a0096f90ab62096f0ff52e62402f
g5sh <eagomez2387@gmail.com>
2020-05-11 21:20:30 +0000

    [ADD] FEATURE: Add point 1 [FIX]: Fix ; symbol

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 /* Write a class Rectangle */
-class Rectangle {
+module.exports = class Rectangle {
     constructor (w, h) {
 	this.width = w;
 	this.height = h;
     }
-}
-
-module.exports = Rectangle;
+};
