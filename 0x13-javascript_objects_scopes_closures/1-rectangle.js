0d5e32288b086b1adf3e110d9db66d19c08db10e
g5sh <eagomez2387@gmail.com>
2020-05-11 21:14:39 +0000

    [ADD] FEATURE: Add point 1

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 /*
-  Write a class Rectangle */
+Write a class Rectangle */
 class Rectangle {
-    constructor(w, h) {
+    constructor (w, h) {
 	this.width = w;
 	this.height = h;
     }
