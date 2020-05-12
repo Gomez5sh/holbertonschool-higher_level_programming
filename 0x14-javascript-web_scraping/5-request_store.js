6b342b2d05d1dd477c26ac6638c5bfb469453c6f
g5sh <eagomez2387@gmail.com>
2020-05-12 17:18:46 -0500

    [ADD] FEATURE: Add point 6

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,14 +5,14 @@ script that gets the contents of a webpage and stores it in a file.
 const request = require('request');
 const fs = require('fs');
 
-let url = process.argv[2];
-let file = process.argv[3];
+const url = process.argv[2];
+const file = process.argv[3];
 
 request(url, function (err, res, body) {
   if (err) {
     console.log(err);
   } else {
-    let content = body;
+    const content = body;
     fs.writeFile(file, content, function (err) {
       if (err) {
         console.log(err);
